f315482aaea85b93b66070cdb4b05aa789c91420
JIN YANG <30322736+jyang526843@users.noreply.github.com>
2020-09-22 16:18:33 -0700

    Add files via upload

--- ba_interp2.cpp
+++ ba_interp2.cpp
@@ -47,12 +47,15 @@
 // GPL
 // (c) 2008 Brian Amberg
 // http://www.brian-amberg.de/
+
+// Modified by Jin Yang, 09/22/2020, only keep the bi-cubic interpolations.
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
 #include <mex.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <math.h>
-#include <string.h>
-#include <iostream>
+// #include <string.h>
+// #include <iostream>
 
 inline 
 static
@@ -61,31 +64,7 @@ int access(int M, int N, int x, int y) {
   if (y<0) y=0; else if (y>=M) y=M-1;
   return M*x + y;
 }
-
-inline
-static
-void indices_linear(
-    int &f00_i,
-    int &f10_i,
-    int &f01_i,
-    int &f11_i,
-    const int x, const int y, 
-    const mwSize &M, const mwSize &N) {
-  if (x<=1 || y<=1 || x>=N-2 || y>=M-2) {
-    f00_i = access(M, N, x,   y  );
-    f10_i = access(M, N, x+1, y  );
-
-    f01_i = access(M, N, x,   y+1);
-    f11_i = access(M, N, x+1, y+1);
-  } else {
-    f00_i = access(M, N, x,   y  );
-    f01_i = f00_i + 1;
-
-    f10_i = f00_i + M;
-    f11_i = f10_i + 1;
-  }
-}
-
+ 
 inline
 static
 void indices_cubic(
@@ -150,116 +129,6 @@ void indices_cubic(
   }
 }
 
-
-template <class REAL>
-static
-void interpolate_nearest(REAL *pO, const REAL *pF, const REAL *pX, const REAL *pY, const mwSize ND, const mwSize M, const mwSize N, const mwSize P) {
-  const mwSize LO = M*N;
-  for (mwSize i=0; i<ND; ++i) {
-    const REAL &x = pX[i];
-    const REAL &y = pY[i];
-
-    const int x_round = int(round(x))-1;
-    const int y_round = int(round(y))-1;
-
-    const int f00_i = access(M, N, x_round, y_round);
-    for (mwSize j=0; j<P; ++j) {
-      pO[i + j*ND] = pF[f00_i + j*LO];
-    }
-  }
-}
-
-template <class REAL, mwSize P>
-static
-void interpolate_nearest_unrolled(REAL *pO, const REAL *pF, const REAL *pX, const REAL *pY, const mwSize ND, const mwSize M, const mwSize N) {
-  const mwSize LO = M*N;
-  for (mwSize i=0; i<ND; ++i) {
-    const REAL &x = pX[i];
-    const REAL &y = pY[i];
-
-    const int x_round = int(round(x))-1;
-    const int y_round = int(round(y))-1;
-
-    const int f00_i = access(M, N, x_round, y_round);
-    for (mwSize j=0; j<P; ++j) {
-      pO[i + j*ND] = pF[f00_i + j*LO];
-    }
-  }
-}
-
-template <class REAL>
-static
-void interpolate_linear(REAL *pO, const REAL *pF, const REAL *pX, const REAL *pY, const mwSize ND, const mwSize M, const mwSize N, const mwSize P) {
-  const mwSize LO = M*N;
-  for (mwSize i=0; i<ND; ++i) {
-    const REAL &x = pX[i];
-    const REAL &y = pY[i];
-
-    const REAL x_floor = floor(x);
-    const REAL y_floor = floor(y);
-
-    const REAL dx = x-x_floor;
-    const REAL dy = y-y_floor;
-
-    const REAL wx0 = 1.0-dx;
-    const REAL wx1 = dx;
-
-
-    const REAL wy0 = 1.0-dy;
-    const REAL wy1 = dy;
-
-    int f00_i, f10_i, f01_i, f11_i;
-
-    indices_linear(
-        f00_i, f10_i, f01_i, f11_i, 
-        int(x_floor-1), int(y_floor-1), M, N);
-
-    for (mwSize j=0; j<P; ++j) {
-
-      pO[i + j*ND] =
-        wy0*(wx0 * pF[f00_i + j*LO] + wx1 * pF[f10_i + j*LO]) +
-        wy1*(wx0 * pF[f01_i + j*LO] + wx1 * pF[f11_i + j*LO]);
-    }
-
-  }
-}
-
-template <class REAL, mwSize P>
-static
-void interpolate_linear_unrolled(REAL *pO, const REAL *pF, const REAL *pX, const REAL *pY, const mwSize ND, const mwSize M, const mwSize N) {
-  const mwSize LO = M*N;
-  for (mwSize i=0; i<ND; ++i) {
-    const REAL &x = pX[i];
-    const REAL &y = pY[i];
-
-    const REAL x_floor = floor(x);
-    const REAL y_floor = floor(y);
-
-    const REAL dx = x-x_floor;
-    const REAL dy = y-y_floor;
-
-    const REAL wx0 = 1.0-dx;
-    const REAL wx1 = dx;
-
-
-    const REAL wy0 = 1.0-dy;
-    const REAL wy1 = dy;
-
-    int f00_i, f10_i, f01_i, f11_i;
-
-    indices_linear(
-        f00_i, f10_i, f01_i, f11_i, 
-        int(x_floor-1), int(y_floor-1), M, N);
-
-    for (mwSize j=0; j<P; ++j) {
-
-      pO[i + j*ND] =
-        wy0*(wx0 * pF[f00_i + j*LO] + wx1 * pF[f10_i + j*LO]) +
-        wy1*(wx0 * pF[f01_i + j*LO] + wx1 * pF[f11_i + j*LO]);
-    }
-
-  }
-}
 
 template <class REAL>
 static
@@ -364,28 +233,28 @@ void interpolate_bicubic_unrolled(REAL *pO, const REAL *pF, const REAL *pX, cons
   }
 }
 
-enum InterpolationMethod { Nearest, Linear, Cubic };
+enum InterpolationMethod { Cubic };
 
-static
-InterpolationMethod parseInterpolationMethod(int nrhs, const mxArray *prhs[]) {
-  if (nrhs<4)
-    return Cubic;
-
-  char method[10] = "cubic    ";
-
-  mxGetString(prhs[3], method, 9);
-
-  if (std::string(method).substr(0, 7).compare("nearest")==0)
-    return Nearest;
-  else if (std::string(method).substr(0, 6).compare("linear")==0)
-    return Linear;
-  else if (std::string(method).substr(0, 5).compare("cubic")==0)
-    return Cubic;
-  else
-    mexErrMsgTxt("Specify one of nearest, linear, cubic as the interpolation method argument.");
-
-  return(Cubic);
-}
+// static
+// InterpolationMethod parseInterpolationMethod(int nrhs, const mxArray *prhs[]) {
+//   if (nrhs<4)
+//     return Cubic;
+// 
+//   char method[10] = "cubic    ";
+// 
+//   mxGetString(prhs[3], method, 9);
+// 
+//   if (std::string(method).substr(0, 7).compare("nearest")==0)
+//     return Nearest;
+//   else if (std::string(method).substr(0, 6).compare("linear")==0)
+//     return Linear;
+//   else if (std::string(method).substr(0, 5).compare("cubic")==0)
+//     return Cubic;
+//   else
+//     mexErrMsgTxt("Specify one of nearest, linear, cubic as the interpolation method argument.");
+// 
+//   return(Cubic);
+// }
 
 template <class REAL> 
 static void interpolate(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
@@ -424,38 +293,9 @@ static void interpolate(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs
   const REAL *pY = (REAL*)mxGetData(prhs[2]);
   REAL       *pO = (REAL*)mxGetData(plhs[0]);  
 
-  switch(parseInterpolationMethod(nrhs, prhs)) {
-    case Nearest:
-      switch (P) {
-        case 1: interpolate_nearest_unrolled<REAL, 1>(pO, pF, pX, pY, ND, M, N); break;
-        case 2: interpolate_nearest_unrolled<REAL, 2>(pO, pF, pX, pY, ND, M, N); break;
-        case 3: interpolate_nearest_unrolled<REAL, 3>(pO, pF, pX, pY, ND, M, N); break;
-        case 4: interpolate_nearest_unrolled<REAL, 4>(pO, pF, pX, pY, ND, M, N); break;
-        case 5: interpolate_nearest_unrolled<REAL, 5>(pO, pF, pX, pY, ND, M, N); break;
-        case 6: interpolate_nearest_unrolled<REAL, 6>(pO, pF, pX, pY, ND, M, N); break;
-        case 7: interpolate_nearest_unrolled<REAL, 7>(pO, pF, pX, pY, ND, M, N); break;
-        case 8: interpolate_nearest_unrolled<REAL, 8>(pO, pF, pX, pY, ND, M, N); break;
-        case 9: interpolate_nearest_unrolled<REAL, 9>(pO, pF, pX, pY, ND, M, N); break;
-        default: 
-                interpolate_nearest<REAL>(pO, pF, pX, pY, ND, M, N, P);
-      }
-      break;
-    case Linear:
-      switch (P) {
-        case 1: interpolate_linear_unrolled<REAL, 1>(pO, pF, pX, pY, ND, M, N); break;
-        case 2: interpolate_linear_unrolled<REAL, 2>(pO, pF, pX, pY, ND, M, N); break;
-        case 3: interpolate_linear_unrolled<REAL, 3>(pO, pF, pX, pY, ND, M, N); break;
-        case 4: interpolate_linear_unrolled<REAL, 4>(pO, pF, pX, pY, ND, M, N); break;
-        case 5: interpolate_linear_unrolled<REAL, 5>(pO, pF, pX, pY, ND, M, N); break;
-        case 6: interpolate_linear_unrolled<REAL, 6>(pO, pF, pX, pY, ND, M, N); break;
-        case 7: interpolate_linear_unrolled<REAL, 7>(pO, pF, pX, pY, ND, M, N); break;
-        case 8: interpolate_linear_unrolled<REAL, 8>(pO, pF, pX, pY, ND, M, N); break;
-        case 9: interpolate_linear_unrolled<REAL, 9>(pO, pF, pX, pY, ND, M, N); break;
-        default: 
-                interpolate_linear<REAL>(pO, pF, pX, pY, ND, M, N, P);
-      }
-      break;
-    case Cubic:
+ // switch(parseInterpolationMethod(nrhs, prhs)) {
+    
+ //   case Cubic:
       switch (P) {
         case 1: interpolate_bicubic_unrolled<REAL, 1>(pO, pF, pX, pY, ND, M, N); break;
         case 2: interpolate_bicubic_unrolled<REAL, 2>(pO, pF, pX, pY, ND, M, N); break;
@@ -469,10 +309,10 @@ static void interpolate(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs
         default: 
                 interpolate_bicubic<REAL>(pO, pF, pX, pY, ND, M, N, P);
       }
-      break;
-    default:
-      mexErrMsgTxt("Unimplemented interpolation method.");
-  }
+  // break;
+  // default:
+  // mexErrMsgTxt("Unimplemented interpolation method.");
+  // }
 }
 
 void mexFunction(int nlhs, mxArray *plhs[],
