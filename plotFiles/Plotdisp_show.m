8d99ae504771013418e9851a07e9e8905c1c7ee6
JIN YANG <30322736+jyang526843@users.noreply.github.com>
2020-11-13 20:02:41 -0800

    Update Plotdisp_show.m

--- plotFiles/Plotdisp_show.m
+++ plotFiles/Plotdisp_show.m
@@ -1,24 +1,30 @@
-% ==============================================
-% function Plotdisp_show
-% ==============================================
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% To plot DIC solved displacement components
+%   1) dispx 
+%   2) dispy
+%
+% Author: Jin Yang  
+% Last date modified: 2020.11.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function Plotdisp_show(U0,coordinatesFEM,elementsFEM)
 
-warning off;
+warning off; 
 
+%% ====== 1) dispx u ======
 figure; show([],elementsFEM(:,1:4),coordinatesFEM,U0(1:2:end)); 
 title('$x-$displacement $u$','FontWeight','Normal','Interpreter','latex');
-view(2); set(gca,'fontsize',18); axis tight; axis equal; colorbar;
-% view([90 -90])
+view(2); set(gca,'fontsize',18); axis tight; axis equal; colorbar; % view([90 -90])
+
 xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter','latex');
 set(gcf,'color','w'); colormap jet;
 a = gca; a.TickLabelInterpreter = 'latex';
 b = colorbar; b.TickLabelInterpreter = 'latex';
 
-colormap jet
-box on
+colormap jet; box on;
 
 
-
+%% ====== 2) dispy v ======
 figure; show([],elementsFEM(:,1:4),coordinatesFEM,U0(2:2:end)); 
 title('$y-$displacement $v$','FontWeight','Normal','Interpreter','latex');
 view(2); set(gca,'fontsize',18); axis tight; axis equal; colorbar;
@@ -28,8 +34,7 @@ set(gcf,'color','w'); colormap jet;
 a = gca; a.TickLabelInterpreter = 'latex';
 b = colorbar; b.TickLabelInterpreter = 'latex';
 
-colormap jet
-box on
+colormap jet; box on;
 
 
 
