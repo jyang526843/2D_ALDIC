277b183ae63228b75d397aec8fe216bbccbd70c0
JIN YANG <30322736+jyang526843@users.noreply.github.com>
2020-11-13 20:03:02 -0800

    Update Plotstrain0.m

--- plotFiles/Plotstrain0.m
+++ plotFiles/Plotstrain0.m
@@ -1,6 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% To plot DIC solved strain components  
+%   1) strain exx
+%   2) strain exy
+%   3) strain eyy
+%   4) principal strain max
+%   5) principal strain min
+%   6) max shear
+%   7) equivalent von Mises strain
+%
+% Author: Jin Yang  
+% Last date modified: 2020.11.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%
 function Plotstrain0(FSubpb3,x,y,sizeOfImg,CurrentImg,OrigDICImgTransparency)
   
-warning off;
+warning off; load('./plotFiles/colormap_RdYlBu.mat','cMap');
 
 M = size(x,1); N = size(x,2);
 u_x = FSubpb3(1:4:end); v_x = FSubpb3(2:4:end);
@@ -8,40 +23,26 @@ u_y = FSubpb3(3:4:end); v_y = FSubpb3(4:4:end);
  
 u_x = reshape(u_x,M,N); v_x = reshape(v_x,M,N);
 u_y = reshape(u_y,M,N); v_y = reshape(v_y,M,N);
-
-% imagesc([x(1,1) x(end,1)], [y(1,1) y(1,end)], flipud(g)); hold on;
-if M < 9, x2 = x(:,1)'; else x2 = linspace(x(1,1),x(end,1),4*(length(x(:,1))-1)+1); x2=x2(:)'; end % x2 = interp(x(:,1)',4);
-if N < 9, y2 = y(1,:); else y2 = linspace(y(1,1),y(1,end),4*(length(y(1,:))-1)+1); y2=y2(:)'; end % y2 = interp(y(1,:),4);
-
-z_exx = gridfit(reshape(x,M*N,1),reshape(y,M*N,1),reshape(u_x,M*N,1),x2,y2);
-
  
+if M < 9,x2 = x(:,1)'; else x2 = interp(x(:,1)',4); end
+if N < 9,y2 = y(1,:); else y2 = interp(y(1,:),4); end
 
+strain_exx = gridfit(reshape(x,M*N,1),reshape(y,M*N,1),reshape(u_x,M*N,1),x2,y2);
+strain_exy = gridfit(reshape(x,M*N,1),reshape(y,M*N,1),reshape(0.5*(v_x+u_y),M*N,1),x2,y2);
+strain_eyy = gridfit(reshape(x,M*N,1),reshape(y,M*N,1),reshape(v_y,M*N,1),x2,y2);
 
-%figure; 
-% contourf(x2,y2,z,10,'EdgeColor','none','LineStyle','none')
-% set(gca,'fontSize',18); view(2);
-% set(gca,'ydir','normal');
-% title('Strain $e_{xx}$','FontWeight','Normal','Interpreter','latex');
-% axis tight; axis equal; colorbar; 
-% if x(M,N) < 200
-%     set(gca,'XTick',[]);
-% end
-% if y(M,N) < 200
-%     set(gca,'YTick',[]);
-% end
-% xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter','latex');
-% set(gcf,'color','w');
-% a = gca; a.TickLabelInterpreter = 'latex';
-% b = colorbar; b.TickLabelInterpreter = 'latex';
-% 
-% colormap jet
-% box on
-
-% alpha(c,.5)
-
+strain_maxshear = sqrt((0.5*(strain_exx-strain_eyy)).^2 + strain_exy.^2);
+% Principal strain
+strain_principal_max = 0.5*(strain_exx+strain_eyy) + strain_maxshear;
+strain_principal_min = 0.5*(strain_exx+strain_eyy) - strain_maxshear;
+% equivalent von Mises strain
+strain_vonMises = sqrt(strain_principal_max.^2 + strain_principal_min.^2 - ...
+             strain_principal_max.*strain_principal_min + 3*strain_maxshear.^2);
+         
+         
+%% ====== 1) Strain exx ====== 
 figure;  
-surf(x2,sizeOfImg(2)+1-y2,z_exx,'EdgeColor','none','LineStyle','none')
+surf(x2,sizeOfImg(2)+1-y2,strain_exx,'EdgeColor','none','LineStyle','none')
 set(gca,'fontSize',18); view(2); box on; set(gca,'ydir','normal');
 title('Strain $e_{xx}$','FontWeight','Normal','Interpreter','latex');
 axis tight; axis equal; colorbar; colormap jet; set(gcf,'color','w');
@@ -51,32 +52,11 @@ xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter
 
 a = gca; a.TickLabelInterpreter = 'latex';
 b = colorbar; b.TickLabelInterpreter = 'latex';
-% alpha(c,.5)
+  
 
-
-
-z_exy = gridfit(reshape(x,M*N,1),reshape(y,M*N,1),reshape(0.5*(v_x+u_y),M*N,1),x2,y2);
-% figure; 
-% contourf(x2,y2,z,10,'EdgeColor','none','LineStyle','none')
-% set(gca,'fontSize',18); view(2); 
-% title('Strain $e_{xy}$','FontWeight','Normal','Interpreter','latex');
-% axis tight; axis equal; colorbar; 
-% if x(M,N) < 200
-%     set(gca,'XTick',[]);
-% end
-% if y(M,N) < 200
-%     set(gca,'YTick',[]);
-% end
-% xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter','latex');
-% set(gcf,'color','w');
-% a = gca; a.TickLabelInterpreter = 'latex';
-% b = colorbar; b.TickLabelInterpreter = 'latex';
-% colormap jet
-% box on
-
-
+%% ====== 2) Strain exy ======
 figure;  
-surf(x2,sizeOfImg(2)+1-y2,z_exy,'EdgeColor','none','LineStyle','none')
+surf(x2,sizeOfImg(2)+1-y2,strain_exy,'EdgeColor','none','LineStyle','none')
 set(gca,'fontSize',18); view(2); box on; set(gca,'ydir','normal');
 title('Strain $e_{xy}$','FontWeight','Normal','Interpreter','latex');
 axis tight; axis equal; colorbar; colormap jet; set(gcf,'color','w');
@@ -86,31 +66,11 @@ xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter
 
 a = gca; a.TickLabelInterpreter = 'latex';
 b = colorbar; b.TickLabelInterpreter = 'latex';
-% alpha(c,.5)
+ 
 
-
-z_eyy = gridfit(reshape(x,M*N,1),reshape(y,M*N,1),reshape(v_y,M*N,1),x2,y2);
-% figure;  
-% contourf(x2,y2,z,10,'EdgeColor','none','LineStyle','none')
-% set(gca,'fontSize',18); view(2); 
-% title('Strain $e_{yy}$','FontWeight','Normal','Interpreter','latex');
-% axis tight; axis equal; colorbar; 
-% if x(M,N) < 200
-%     set(gca,'XTick',[]);
-% end
-% if y(M,N) < 200
-%     set(gca,'YTick',[]);
-% end
-% xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter','latex');
-% set(gcf,'color','w');
-% a = gca; a.TickLabelInterpreter = 'latex';
-% b = colorbar; b.TickLabelInterpreter = 'latex';
-% colormap jet
-% box on
-
-
+%% ====== 3) Strain eyy ======
 figure;  
-surf(x2,sizeOfImg(2)+1-y2,z_eyy,'EdgeColor','none','LineStyle','none')
+surf(x2,sizeOfImg(2)+1-y2,strain_eyy,'EdgeColor','none','LineStyle','none')
 set(gca,'fontSize',18); view(2); box on; set(gca,'ydir','normal');
 title('Strain $e_{yy}$','FontWeight','Normal','Interpreter','latex');
 axis tight; axis equal; colorbar; colormap jet; set(gcf,'color','w');
@@ -120,13 +80,39 @@ xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter
 
 a = gca; a.TickLabelInterpreter = 'latex';
 b = colorbar; b.TickLabelInterpreter = 'latex';
-% alpha(c,.5)
+ 
 
-
-
+%% ====== 4) Strain e_principal_max ======
 figure;  
-z_maxshear = sqrt((0.5*(z_exx-z_eyy)).^2 + z_exy.^2);
-surf(x2,sizeOfImg(2)+1-y2, z_maxshear ,'EdgeColor','none','LineStyle','none')
+surf(x2,sizeOfImg(2)+1-y2,strain_principal_max,'EdgeColor','none','LineStyle','none')
+set(gca,'fontSize',18); view(2); box on; set(gca,'ydir','normal');
+title('Principal strain $e_{\max}$','FontWeight','Normal','Interpreter','latex');
+axis tight; axis equal; colorbar; colormap jet; set(gcf,'color','w');
+if x(M,N) < 200,set(gca,'XTick',[]); end
+if y(M,N) < 200,set(gca,'YTick',[]); end
+xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter','latex');
+
+a = gca; a.TickLabelInterpreter = 'latex';
+b = colorbar; b.TickLabelInterpreter = 'latex';
+
+
+%% ====== 5) Strain e_principal_min ======
+figure;  
+surf(x2,sizeOfImg(2)+1-y2,strain_principal_min,'EdgeColor','none','LineStyle','none')
+set(gca,'fontSize',18); view(2); box on; set(gca,'ydir','normal');
+title('Principal strain $e_{\min}$','FontWeight','Normal','Interpreter','latex');
+axis tight; axis equal; colorbar; colormap jet; set(gcf,'color','w');
+if x(M,N) < 200,set(gca,'XTick',[]); end
+if y(M,N) < 200,set(gca,'YTick',[]); end
+xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter','latex');
+
+a = gca; a.TickLabelInterpreter = 'latex';
+b = colorbar; b.TickLabelInterpreter = 'latex';
+
+
+%% ====== 6) Strain e_max_shear ======
+figure;  
+surf(x2,sizeOfImg(2)+1-y2, strain_maxshear ,'EdgeColor','none','LineStyle','none')
 set(gca,'fontSize',18); view(2); box on; set(gca,'ydir','normal');
 title('Max shear strain','FontWeight','Normal','Interpreter','latex');
 axis tight; axis equal; colorbar; colormap jet; set(gcf,'color','w');
@@ -136,8 +122,20 @@ xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter
 
 a = gca; a.TickLabelInterpreter = 'latex';
 b = colorbar; b.TickLabelInterpreter = 'latex';
-% alpha(c,.5)
+ 
 
+%% ====== 7) von Mises equivalent strain ======
+figure;  
+surf(x2,sizeOfImg(2)+1-y2, strain_vonMises ,'EdgeColor','none','LineStyle','none')
+set(gca,'fontSize',18); view(2); box on; set(gca,'ydir','normal');
+title('von Mises equivalent strain','FontWeight','Normal','Interpreter','latex');
+axis tight; axis equal; colorbar; colormap jet; set(gcf,'color','w');
+if x(M,N) < 200,set(gca,'XTick',[]); end
+if y(M,N) < 200,set(gca,'YTick',[]); end
+xlabel('$x$ (pixels)','Interpreter','latex'); ylabel('$y$ (pixels)','Interpreter','latex');
 
+a = gca; a.TickLabelInterpreter = 'latex';
+b = colorbar; b.TickLabelInterpreter = 'latex';
 
  
+
